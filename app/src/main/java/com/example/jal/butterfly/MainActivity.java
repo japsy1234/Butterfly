c0758d7f468452a2a310e8aa79823da7c4c663f4
japsy123 <jal08panchal@gmail.com>
2017-06-11 22:20:13 -0700

    Third Commit

--- app/src/main/java/com/example/jal/butterfly/MainActivity.java
+++ app/src/main/java/com/example/jal/butterfly/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jal.butterfly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button first_button;
+
+    public void init(){
+        first_button = (Button)findViewById(R.id.explore);
+        first_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explore = new Intent(MainActivity.this,Choices.class);
+                startActivity(explore);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
     }
 
